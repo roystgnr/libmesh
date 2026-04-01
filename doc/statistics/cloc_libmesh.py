4dec58b35eab74781ae7692ddd7c8ba9e1b5fa59
John Peterson <jwpeterson@gmail.com>
2026-04-01 13:36:33 -0500

    cloc_libmesh.py: Update plot tick marks

--- doc/statistics/cloc_libmesh.py
+++ doc/statistics/cloc_libmesh.py
@@ -362,7 +362,7 @@ ax1.plot(date_nums, n_files, color=u'#4878cf', marker=None, linestyle='--', mark
 ax1.set_ylabel('Files (blue dashed)')
 
 # Set up x-tick locations
-ticks_names = ['2003', '2007', '2011', '2015', '2019', '2023']
+ticks_names = ['2003', '2007', '2011', '2015', '2019', '2023', '2027']
 
 # Get numerical values for the names
 tick_nums = []
