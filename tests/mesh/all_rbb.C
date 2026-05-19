dac29cf48b5933d79bc78eafed6affeee690d5f7
Roy Stogner <Roy.Stogner@inl.gov>
2026-05-19 10:44:07 -0500

    DO NOT MERGE - checkpoint
    Still working on calibrating and verifying these sphere tests.

--- tests/mesh/all_rbb.C
+++ tests/mesh/all_rbb.C
@@ -57,6 +57,14 @@ public:
   CPPUNIT_TEST( testAllRBBHex27 );
   CPPUNIT_TEST( testAllRBBPrism6 );
   CPPUNIT_TEST( testAllRBBPrism18 );
+
+  CPPUNIT_TEST( testAllRBBSphere6 );
+  CPPUNIT_TEST( testAllRBBSphere24 );
+  CPPUNIT_TEST( testAllRBBSphere96 );
+
+  CPPUNIT_TEST( testAllRBBTri6Sphere12 );
+  CPPUNIT_TEST( testAllRBBTri6Sphere48 );
+  CPPUNIT_TEST( testAllRBBTri6Sphere192 );
 #endif
 
   CPPUNIT_TEST_SUITE_END();
@@ -228,6 +236,168 @@ protected:
                             area, max_rbb_error);
   }
 
+  void test_sphere(unsigned int n_refinements, const ElemType type = QUAD9)
+  {
+    Mesh interior_mesh(*TestCommWorld),
+         boundary_mesh(*TestCommWorld);
+
+    const Real radius = 1;
+    const Real surface_area = 4 * pi * radius * radius;
+    const Real tol = TOLERANCE*TOLERANCE;
+
+    // Build a filled sphere
+    MeshTools::Generation::build_sphere (interior_mesh, radius,
+                                         n_refinements, HEX27);
+
+    // Get just the outer QUAD9 sphere mesh
+    interior_mesh.get_boundary_info().sync(boundary_mesh);
+
+    const dof_id_type n_faces = 6 << (2*n_refinements);
+
+    CPPUNIT_ASSERT_EQUAL(boundary_mesh.n_elem(), n_faces);
+
+    auto check_radii = [&boundary_mesh, radius, type](Real radius_tol) {
+      constexpr int n_intervals = 40;
+      std::cerr << "mesh.n_elem() = " << boundary_mesh.n_elem() << std::endl;
+
+      Real max_radius_error = 0;
+      for (auto & elem : boundary_mesh.element_ptr_range())
+        {
+          // We can't necessarily assert that each Node is at a
+          // specified radius from the circle center, because these
+          // may be spline control nodes.  We want physical points
+          // within an element to all be at the desired radius, but on
+          // rational quadratics that's only possible for "latitude /
+          // longitude" quad edges, so we need a non-trivial tolerance
+          // here.
+          Point master_pt;
+          if (type == TRI6)
+            {
+              for (master_pt(0) = 0; master_pt(0) <= 1 + TOLERANCE;
+                   master_pt(0) += Real(1)/n_intervals)
+                {
+                  for (master_pt(1) = 0; master_pt(1) <= 1 - master_pt(0) + TOLERANCE;
+                       master_pt(1) += Real(1)/n_intervals)
+                    {
+                      const Point p = FEMap::map(elem->dim(), elem, master_pt);
+                      max_radius_error = std::max(max_radius_error, std::abs(radius-p.norm()));
+                    }
+                }
+            }
+          else
+            {
+              libmesh_assert_equal_to(type, QUAD9);
+              for (master_pt(0) = -1; master_pt(0) <= 1 + TOLERANCE;
+                   master_pt(0) += Real(2)/n_intervals)
+                {
+                  for (master_pt(1) = -1; master_pt(1) <= 1 + TOLERANCE;
+                       master_pt(1) += Real(2)/n_intervals)
+                    {
+                      const Point p = FEMap::map(elem->dim(), elem, master_pt);
+                      max_radius_error = std::max(max_radius_error, std::abs(radius-p.norm()));
+                    }
+                }
+            }
+          LIBMESH_ASSERT_FP_EQUAL(max_radius_error, 0, radius_tol);
+        }
+    };
+
+    auto verify_equispaced_midnodes = [&boundary_mesh]()
+    {
+      std::unique_ptr<Elem> side_ptr;
+      for (auto & elem : boundary_mesh.element_ptr_range())
+        {
+          for (auto s : elem->side_index_range())
+            {
+              elem->build_side_ptr(side_ptr, s);
+
+              auto c02 = side_ptr->point(2) - side_ptr->point(0);
+              auto c12 = side_ptr->point(2) - side_ptr->point(1);
+              LIBMESH_ASSERT_FP_EQUAL
+                (c02.norm_sq(), c12.norm_sq(), TOLERANCE*TOLERANCE);
+            }
+
+          if (elem->type() == QUAD9)
+            {
+              auto c08 = elem->point(8) - elem->point(0);
+              auto c18 = elem->point(8) - elem->point(1);
+              auto c28 = elem->point(8) - elem->point(2);
+              auto c38 = elem->point(8) - elem->point(3);
+              libmesh_assert_less
+                (std::abs(c08.norm_sq() - c18.norm_sq()), TOLERANCE*TOLERANCE);
+              libmesh_assert_less
+                (std::abs(c08.norm_sq() - c28.norm_sq()), TOLERANCE*TOLERANCE);
+              libmesh_assert_less
+                (std::abs(c08.norm_sq() - c38.norm_sq()), TOLERANCE*TOLERANCE);
+            }
+        }
+    };
+
+    verify_equispaced_midnodes();
+
+    if (type == TRI6)
+      {
+        MeshTools::Modification::all_tri(boundary_mesh);
+        verify_equispaced_midnodes();
+      }
+    else
+      libmesh_assert_equal_to(type, QUAD9);
+
+    // For our sphere construction, all of our Lagrange nodes should
+    // be at exactly the right radius.
+    for (auto & node : boundary_mesh.node_ptr_range())
+      {
+        const Point p = *node;
+        LIBMESH_ASSERT_FP_EQUAL(p.norm(), radius, tol);
+      }
+
+    // But Lagrange isn't isogeometric, so non-nodes should have
+    // radius error.  Empirically, our error looks like Ch^3
+    const Real max_lagrange_rad_error =
+      radius * .2 / (1 << (3*n_refinements));
+    check_radii(max_lagrange_rad_error);
+
+    // We just did Lagrange interpolation, so our mesh measure
+    // shouldn't be *quite* right.  Empirically, we converge from
+    // beneath, and our error looks like Ch^4.
+    const Real max_lagrange_vol_error =
+      radius * radius * 1.5 / (1 << (4*n_refinements));
+    LIBMESH_ASSERT_FP_EQUAL(MeshTools::volume(boundary_mesh),
+                            surface_area, max_lagrange_vol_error);
+
+    MeshTools::Modification::all_rbb(boundary_mesh);
+
+    // All of our vertices should still be at exactly the right
+    // radius; for these the control point is the point.
+    for (auto & elem : boundary_mesh.element_ptr_range())
+      {
+        CPPUNIT_ASSERT_EQUAL(RATIONAL_BERNSTEIN_MAP, elem->mapping_type());
+
+        for (auto v : make_range(elem->n_vertices()))
+          {
+            const Point & p = elem->point(v);
+            LIBMESH_ASSERT_FP_EQUAL(p.norm(), radius, tol);
+          }
+      }
+
+    // We're not building a stereographic mesh on the sphere, so we
+    // still have error ... of roughly the same order???
+    /*
+    const Real max_rbb_rad_error =
+      radius * 0.1 / (1 << (4*n_refinements));
+    check_radii(max_rbb_rad_error);
+    */
+
+    // We're using quadrature for volume approximation, so we still
+    // have error, but our quadrature error looks something like Ch^6
+    // with a much smaller C. ???
+    const Real max_rbb_vol_error = 0;
+      //radius * 1e-3 / (1 << (6*n_refinements));
+    LIBMESH_ASSERT_FP_EQUAL(MeshTools::volume(boundary_mesh),
+                            surface_area, max_rbb_vol_error);
+  }
+
+
 public:
   void setUp() {}
 
@@ -266,6 +436,17 @@ public:
   void testAllRBBTri6Disk10() { LOG_UNIT_TEST; test_disk(0, TRI6); }
   void testAllRBBTri6Disk40() { LOG_UNIT_TEST; test_disk(1, TRI6); }
   void testAllRBBTri6Disk160() { LOG_UNIT_TEST; test_disk(2, TRI6); }
+
+  // 0 refinements of our default sphere gives us 6 RBB quad faces or
+  // 12 RBB triangles
+  void testAllRBBSphere6() { LOG_UNIT_TEST; test_sphere(0); }
+  void testAllRBBSphere24() { LOG_UNIT_TEST; test_sphere(1); }
+  void testAllRBBSphere96() { LOG_UNIT_TEST; test_sphere(2); test_sphere(3); test_sphere(4); test_sphere(5); }
+
+  void testAllRBBTri6Sphere12() { LOG_UNIT_TEST; test_sphere(0, TRI6); }
+  void testAllRBBTri6Sphere48() { LOG_UNIT_TEST; test_sphere(1, TRI6); }
+  void testAllRBBTri6Sphere192() { LOG_UNIT_TEST; test_sphere(2, TRI6); test_sphere(3, TRI6); test_sphere(4, TRI6); test_sphere(5, TRI6); }
+
 };
 
 
